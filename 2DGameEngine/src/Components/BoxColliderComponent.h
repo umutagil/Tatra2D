7f20fb2867b1bc02bc533204a7995dab9f7a3fb1
Umut Agil <umut.agil@scssoft.com>
2023-04-16 10:34:10 +0200

    added documentation to box collider for use of scale in collision boxes

--- 2DGameEngine/src/Components/BoxColliderComponent.h
+++ 2DGameEngine/src/Components/BoxColliderComponent.h
@@ -2,6 +2,13 @@
 
 #include <glm/glm.hpp>
 
+/**
+ * @brief Component that stores collision box parameters.
+ * 
+ * Currently entity scale is not reflected on the collider automatically,
+ * it needs to be done manually.
+ * TODO: Find a way to set it easily.
+ */
 struct BoxColliderComponent
 {
 	BoxColliderComponent(const int width = 0, const int height = 0, const glm::vec2 offset = glm::vec2(0));
