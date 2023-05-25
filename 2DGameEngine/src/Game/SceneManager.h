570bfc7b74a0d4ce445c31c83d2dc4c6d7ad5f93
Umut Agil <umut.agil@scssoft.com>
2023-05-25 22:58:49 +0200

    added screen-world coordinate conversion helpers

--- 2DGameEngine/src/Game/SceneManager.h
+++ 2DGameEngine/src/Game/SceneManager.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <string>
+#include <glm/glm.hpp>
+#include <SDL.h>
 
 struct TileProperties
 {
@@ -23,14 +25,32 @@ public:
 	const TileProperties& GetActiveTile();
 	const bool HasActiveTile();
 
+	inline const SDL_Rect& GetCamera() const;
 	void AddTile(const TileProperties& tileProperties);
+	inline glm::ivec2 ScreenToWorld(const glm::ivec2& screenPos) const;
+	inline glm::ivec2 WorldToScreen(const glm::ivec2& worldPos) const;
 
 private:
+	const SDL_Rect& camera;
 	TileProperties activeTile;
 
 };
 
 
+inline const SDL_Rect& SceneManager::GetCamera() const
+{
+	return camera;
+}
+
+inline glm::ivec2 SceneManager::ScreenToWorld(const glm::ivec2& screenPos) const
+{
+	return glm::ivec2(screenPos.x + camera.x, screenPos.y + camera.y);
+}
+
+inline glm::ivec2 SceneManager::WorldToScreen(const glm::ivec2& worldPos) const
+{
+	return glm::ivec2(worldPos.x - camera.x, worldPos.y - camera.y);
+}
 
 
 
