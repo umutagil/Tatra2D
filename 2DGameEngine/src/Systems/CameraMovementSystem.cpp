6fae078b51e16fd08d60cd997466bbf8f1c0a898
Umut Agil <umut.agil@scssoft.com>
2023-05-28 15:07:37 +0200

    fix camera movement for the maps that are smaller than camera view dimensions

--- 2DGameEngine/src/Systems/CameraMovementSystem.cpp
+++ 2DGameEngine/src/Systems/CameraMovementSystem.cpp
@@ -17,10 +17,14 @@ void CameraMovementSystem::Update(SDL_Rect& camera)
 	for (Entity entity : GetSystemEntities()) {
 		TransformComponent& transform = entity.GetComponent<TransformComponent>();
 
-		const int cameraX = static_cast<int>(transform.position.x) - (camera.w / 2);
-		const int cameraY = static_cast<int>(transform.position.y) - (camera.h / 2);
-
-		camera.x = std::clamp(cameraX, 0, Game::mapWidth - camera.w);
-		camera.y = std::clamp(cameraY, 0, Game::mapHeight - camera.h);
+		if (camera.w < Game::mapWidth) {
+			const int cameraX = static_cast<int>(transform.position.x) - (camera.w / 2);
+			camera.x = std::clamp(cameraX, 0, Game::mapWidth - camera.w);
+		}
+		
+		if (camera.h < Game::mapHeight) {
+			const int cameraY = static_cast<int>(transform.position.y) - (camera.h / 2);
+			camera.y = std::clamp(cameraY, 0, Game::mapHeight - camera.h);
+		}
 	}
 }
